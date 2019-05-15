5258884dd6f4cd7f125e5d3b7aefbee6e077e6a0
adityagosavi7 <50235320+adityagosavi7@users.noreply.github.com>
2019-05-15 18:04:59 +0530

    Update web.php
    Update Web.php

--- routes/web.php
+++ routes/web.php
@@ -113,6 +113,11 @@ Route::group(['middleware' => 'disablepreventback'],function()
 	Route::get('/bidsList/','ProjectController@bidsList')->name('bidsList')->middleware(guest::class);
 	Route::get('/userProjects/','ProjectController@usersProject')->name('usersProject')->middleware(guest::class);
 	Route::get('/siteAddress/{id}','ProjectController@viewMap')->name('siteAddress')->middleware(guest::class);
+	
+	/* =========== PROJECT MANAGER EDIT PAGE ROUTE ================ */
+	Route::get('/editProjectManager/{id}','UserController@editProjectManager')->name('editProjectManager')->middleware(guest::class);
+	Route::post('/updateProjectManager/{id}', 'UserController@updateProjectManager')->name('updateProjectManager')->middleware(guest::class);
+	Route::post('/checkProjectManagerEmail/{id}', 'UserController@checkProjectManagerEmail')->name('checkProjectManagerEmail')->middleware(guest::class);
 
 });
 Route::get('/forgotPassword/{userid}','LoginController@forgotpassword')->name('forgotPassword');
