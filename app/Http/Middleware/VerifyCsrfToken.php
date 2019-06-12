1cf2a37884351708d9938e0944d9bc3cb78fdd00
adityagosavi7 <50235320+adityagosavi7@users.noreply.github.com>
2019-06-12 18:55:00 +0530

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,8 +14,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except_urls = [
-       // 'checkProjectManagerEmail',
-        '*'
+       'checkProjectManagerEmail'
       
     ];
 
